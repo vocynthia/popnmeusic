49dc1f1d1458335a947996427cc08d6c54a1df56
froo <>
2023-01-18 09:12:31 +0000

    in case

--- music_cog.py
+++ music_cog.py
@@ -208,3 +208,19 @@ class music_cog(commands.Cog):
           await ctx.send(embed=embed)
 
 ##leave due to inactvity
+#    async def on_voice_state_update(self, member, before, after):
+#        if not member.id == self.bot.user.id:
+#          return
+
+#        elif before.channel is None:
+#          voice = after.channel.guild.voice_client
+#          time = 0
+#          while True:
+#              await asyncio.sleep(1)
+#              time = time + 1
+#              if voice.is_playing() and not voice.is_paused():
+#                time = 0
+#              if time == 600:
+#                await voice.disconnect()
+#              if not voice.is_connected():
+#                break
